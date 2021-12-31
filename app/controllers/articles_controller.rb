f02cae51e16fa69ac19a53333278bbca9a96c696
hassanali1103 <hassanali1103@hotmail.com>
2021-12-31 15:19:19 -0500

    Added create & new wth flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,21 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article created successfully"
+            redirect_to @article
+        else
+            render 'new'
+        end    
+    end
+
+    def new
+        @article = Article.new
+    end
+
+
 
 
 end
