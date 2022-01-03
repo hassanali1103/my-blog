bcaa70d4279104aebe74a7be5fe81610fa041605
hassanali1103 <hassanali1103@hotmail.com>
2022-01-02 23:28:18 -0500

    Users model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_072546) do
+ActiveRecord::Schema.define(version: 2022_01_03_035314) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
