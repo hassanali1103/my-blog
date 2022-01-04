adab977e591244e904c368311fb0b2129e6237d5
hassanali1103 <hassanali1103@hotmail.com>
2022-01-03 21:31:13 -0500

    User profile page complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def new
         @user = User.new
     end
@@ -12,6 +17,21 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_param)
+            flash[:notice] = "Account updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+
     end
 
     private
