a486efca42fcc3c59b75870077e88b1baca49d62
hassanali1103 <hassanali1103@hotmail.com>
2022-01-12 21:31:27 -0500

    Pagination added to Article index, User Index and User profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -27,7 +31,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_param)
             flash[:notice] = "Account updated successfully"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
